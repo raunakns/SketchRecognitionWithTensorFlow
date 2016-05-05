a037a8d2ab3c96bf391b82541cbede6ded5f62af
dtracers <dtracers@gmail.com>
2016-05-05 11:02:45 -0500

    got all three codes working

--- tensorFlow/main/src/python/test1.py
+++ tensorFlow/main/src/python/test1.py
@@ -2,10 +2,13 @@ import tensorflow as tf
 import numpy as np
 import math
 
+tf.reset_default_graph()
+
 X = tf.constant([1, 0])
 Y = tf.constant([0, 1])
 BOTH = tf.constant([1, 1])
-WORKING = tf.constant([1])
+WORKING = tf.constant(1)
+PI = tf.constant(math.pi)
 
 def computeCentroid(tf, points):
     """Computes the centroid of the points."""
@@ -36,62 +39,32 @@ def create_no_op_func(tensor):
 
 def stretch(tf, points, dim, amount):
     """points is a 2 by ??? tensor, dim is a 1 by 2 tensor, amount is tensor scalor"""
-    x_list, y_list = tf.split(0, 2, points)
+    x_list, y_list = tf.split(1, 2, points)
     x_stretch, y_stretch = tf.split(0, 2, dim)
-    #x_list_stretched = tf.cond(tf.equal(x_stretch, WORKING, name="is_stretch_x"),
-    #                           create_mult_func(tf, amount, x_list), create_no_op_func(x_list))
-    #y_list_stretched = tf.cond(tf.equal(y_stretch, WORKING, name="is_stretch_Y"),
-    #
-    return tf.pack(x_list_stretched, y_list_stretched)
+    is_stretch_X = tf.equal(x_stretch, WORKING, name="is_stretch_x")
+    is_stretch_Y = tf.equal(y_stretch, WORKING, name="is_stretch_Y")
+    x_list_stretched = tf.cond(tf.reshape(is_stretch_X, []),
+                               create_mult_func(tf, amount, x_list), create_no_op_func(x_list))
+    y_list_stretched = tf.cond(tf.reshape(is_stretch_Y, []),
+                               create_mult_func(tf, amount, y_list), create_no_op_func(y_list))
+    return tf.concat(1, [x_list_stretched, y_list_stretched])
 
 
 def stretch_around_center(tf, points, dim, amount):
     """Stretches the points around their center"""
     centroid = computeCentroid(tf, points)
-    return stretch(tf, points - centroid, dim, amount) + centroid
-tf.reset_default_graph()
+    return stretch(tf, points- centroid, dim, amount) + centroid
+
 example_points = np.array([[1, 1], [2, 2], [3, 3]], dtype=np.float32)
 example_point_list = tf.placeholder(tf.float32)
-PI = tf.constant(math.pi)
 
-# centroid = tf.reduce_mean(example_point_list, 0)
-
+#result = move_to_center(tf, example_point_list)
 #result = rotate_around_center(tf, example_point_list, PI)
-result = stretch_around_center(tf, example_point_list, X, 1)
+result = stretch_around_center(tf, example_point_list, X, tf.constant(2.))
 
-# Create a Constant op that produces a 1x2 matrix.  The op is
-# added as a node to the default graph.
-#
-# The value returned by the constructor represents the output
-# of the Constant op.
-matrix1 = tf.constant([[1., 1.],
-                       [2., 2.],
-                       [3., 3.]])
-
-# Create another Constant that produces a 2x1 matrix.
-matrix2 = tf.constant([[2.],[2.]])
-theta = tf.placeholder(tf.float32, shape=(1, 1))
-
-# Create a Matmul op that takes 'matrix1' and 'matrix2' as inputs.
-# The returned value, 'product', represents the result of the matrix
-# multiplication.
-summary_op = tf.merge_all_summaries()
-# Launch the default graph.
 sess = tf.Session()
-
-# To run the matmul op we call the session 'run()' method, passing 'product'
-# which represents the output of the matmul op.  This indicates to the call
-# that we want to get the output of the matmul op back.
-#
-# All inputs needed by the op are run automatically by the session.  They
-# typically are run in parallel.
-#
-# The call 'run(product)' thus causes the execution of three ops in the
-# graph: the two constants and matmul.
-#
-# The output of the op is returned in 'result' as a numpy `ndarray` object.
 with tf.Session() as sess:
-    writer = tf.train.SummaryWriter('../../../../log', sess.graph)
+    # writer = tf.train.SummaryWriter('../../../../log', sess.graph)
     result = sess.run(result, feed_dict={example_point_list: example_points})
     print(result)
 # ==> [[ 12.]]
